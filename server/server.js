ccf7f67741deebeb01530d767b07fbbeee57a86a
Piyush Dubey <piyushdubey924@gmail.com>
2025-08-08 19:11:32 +0530

    final project

--- server/server.js
+++ server/server.js
@@ -56,6 +56,8 @@ app.get("/restaurants/:resId", async (req, res) => {
   }
 });
 
-app.listen(3001, () =>
-  console.log("Proxy server running at http://localhost:3001")
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () =>
+  console.log(`Proxy server running at http://localhost:${PORT}`)
 );
